fff3caa95f73c6ada9fd99152569d0a1deacfaa8
dtklumpp <dtklumpp@gmail.com>
2020-10-19 23:22:42 -0400

    adds UI for Search

--- src/Home.js
+++ src/Home.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import logo from './logo.svg';
+import Search from './components/Search/Search.js';
 
 function Home() {
   return <div>
     <h2>Hello World</h2>
     <img src="{logo}" alt=""/>
+    <Search />
   </div>
 }
 
---
+++ src/components/Search/Search.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Search.css';
+
+function Search() {
+    return <div>
+            <input type="text" placeholder="search input"/>
+        </div>
+}
+
+export default Search;
