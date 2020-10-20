2077d9e70e4edf58690b899f0278807ad9dae878
dtklumpp <dtklumpp@gmail.com>
2020-10-19 23:13:37 -0400

    setup basic file structure and app and index files

---
+++ src/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+import logo from './logo.svg';
+
+function Home() {
+  return <div>
+    <h2>Hello World</h2>
+    <img src="{logo}" alt=""/>
+  </div>
+}
+
+export default Home;
